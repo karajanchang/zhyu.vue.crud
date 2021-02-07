a047597e4d2ea89cbbb272c4da85a9d410abb2ec
david <karajanchang@gmail.com>
2021-02-07 14:42:54 +0800

    [整合後台必需的 controller / repository / model / config / css /js ] config設定檔加入

---
+++ src/config/columns/admin/system/menu.php
@@ -0,0 +1,146 @@
+<?php
+
+return [
+    'model' =>App\Models\Menu::class,
+    'title' => '選單管理',
+    'index_buttons' => [
+        'draggable' => true,
+        'width' => 250,
+        [
+            'label' => '修改',
+            'url' => '/admin/system/menu/{id}/edit',
+            'css' => 'is-info',
+            'type'=> 'update'
+        ],
+        [
+            'label' => '刪除',
+            'url' => '/admin/system/menu/{id}',
+            'css' => 'is-danger',
+            'type'=> 'delete'
+        ],
+        [
+            'label' => '下層',
+            'url' => '/admin/system/menu?parent_id={id}',
+            'css' => 'is-primary',
+        ],
+    ],
+    'form_buttons' => [
+        [
+            'label' => '取消',
+            'url' => '/admin/system/menu',
+            'css' => 'is-default',
+            'type'=> 'cancel'
+        ],
+        [
+            'label' => '送出',
+            'css' => 'is-primary',
+            'type'=> 'submit'
+        ],
+    ],
+    'columns' => [
+        'title' =>
+            [
+                'name' => '名稱',
+                'type' => 'text',
+                'rules' => [
+                    'store' =>  'required|string',
+                    'update' =>  'required|string',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                    'header-class' => '',
+                    'cell-class' => '',
+                ],
+            ],
+        'ctitle' =>
+            [
+                'name' => '顯示文字',
+                'type' => 'text',
+                'rules' => [
+                    'store' =>  'required|string',
+                    'update' =>  'required|string',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                    'header-class' => '',
+                    'cell-class' => '',
+                ],
+            ],
+        'url' =>
+            [
+                'name' => '網址',
+                'type' => 'text',
+                'rules' => [
+                    'store' =>  'string',
+                    'update' =>  'string',
+                ],
+                'params' => [
+                    'header-class' => '',
+                    'cell-class' => '',
+                ],
+            ],
+        'icon_pack' =>
+            [
+                'name' => 'icon種類',
+                'type' => 'text',
+                'rules' => [
+                    'store' =>  'string',
+                    'update' =>  'string',
+                ],
+            ],
+        'icon' =>
+            [
+                'name' => 'icon名稱',
+                'type' => 'text',
+                'rules' => [
+                    'store' =>  'string',
+                    'update' =>  'string',
+                ],
+            ],
+        'orderby' =>
+            [
+                'name' => '排序',
+                'type' => 'integer',
+                'rules' => [
+                    'store' =>  'integer',
+                    'update' =>  'integer',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                    'header-class' => '',
+                    'cell-class' => '',
+                ],
+            ],
+        'is_online' =>
+            [
+                'name' => '是否上線',
+                'type' => 'checkbox',
+                'rules' => [
+                    'store' =>  'nullable|integer',
+                    'update' =>  'nullable|integer',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                    'header-class' => '',
+                    'cell-class' => '',
+                ],
+            ],
+        'parent_id' =>
+            [
+                'name' => '排序',
+                'type' => 'hidden',
+                'rules' => [
+                    'store' =>  'nullable|integer',
+                    'update' =>  'nullable|integer',
+                ],
+            ],
+
+    ],
+    'joins' => [
+        /*
+        'fac_members_id' => [
+            'members', 'posts.members_id', '=', 'members.id'
+        ],
+        */
+    ],
+];
---
+++ src/config/columns/admin/system/page.php
@@ -0,0 +1,119 @@
+<?php
+
+return [
+    'model' => \App\Models\Page::class,
+    'title' => '頁面管理',
+    'index_buttons' => [
+        'width' => 200,
+        'update' => [
+            'label' => '修改',
+            'url' => '/admin/system/page/{id}/edit',
+            'css' => 'is-info',
+            'type'=> 'update'
+        ],
+        'delete' => [
+            'label' => '刪除',
+            'url' => '/admin/system/page/{id}',
+            'css' => 'is-danger',
+            'type'=> 'delete'
+        ],
+        [
+            'label' => '頁面管理',
+            'url' => '/admin/system/pagecontent/{id}/index',
+            'css' => 'is-primary',
+            'type' => 'button',
+        ],
+    ],
+    'form_buttons' => [
+        [
+            'label' => '取消',
+            'url' => '/admin/system/page',
+            'css' => 'is-default',
+            'type'=> 'cancel'
+        ],
+        [
+            'label' => '送出',
+            'css' => 'is-primary',
+            'type'=> 'submit'
+        ],
+    ],
+    'columns' => [
+        'title' =>
+            [
+                'name' => '頁面主題',
+                'type' => 'text',
+                'rules' => [
+                    'store' =>  'required|string',
+                    'update' =>  'required|string',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                ],
+            ],
+        'banner' =>
+            [
+                'name' => '頁面Banner',
+                'type' => 'image', //---file or image
+                'rules' => [
+                    'store' =>  'nullable|image',
+                    'update' =>  'nullable|image',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                ],
+            ],
+        'uri' =>
+            [
+                'name' => '網址uri',
+                'type' => 'text',
+                'rules' => [
+                    'store' =>  'string',
+                    'update' =>  'string',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                ],
+            ],
+        'menu_id' =>
+            [
+                'name' => '選單',
+                'type' => 'select',
+                'rules' => [
+                    'store' =>  'integer',
+                    'update' =>  'integer',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                    'header-class' => '',
+                    'cell-class' => '',
+                ],
+                'relation' => [
+                    'table' => 'menus',
+                    'name' => 'menu',
+                    'column' => 'ctitle',
+                ],
+            ],
+        'is_online' =>
+            [
+                'name' => '是否有效',
+                'type' => 'checkbox',
+                'rules' => [
+                    'store' =>  'integer',
+                    'update' =>  'integer',
+                ],
+                'params' => [
+                    'sortable', 'searchable',
+                    'header-class' => '',
+                    'cell-class' => '',
+                ],
+                'display_index' => true,
+                'display_form' => true,
+            ],
+    ],
+    'joins' => [
+
+        'menu_id' => [
+            'menus', 'pages.menu_id', '=', 'menus.id'
+        ],
+    ],
+];
