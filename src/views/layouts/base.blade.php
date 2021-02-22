b9dde4039217ffb01ffae6626425986b5a606414
david <karajanchang@gmail.com>
2021-02-22 11:48:37 +0800

    change route index to home

--- src/views/layouts/base.blade.php
+++ src/views/layouts/base.blade.php
@@ -52,7 +52,7 @@ to get the desired effect
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="{{ route('index') }}" class="brand-link">
+            <a href="{{ route('home') }}" class="brand-link">
                 <img src="{{ asset('vendor/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                      style="opacity: .8">
                 <span class="brand-text font-weight-light">{{ env('APP_NAME', 'System') }}</span>
