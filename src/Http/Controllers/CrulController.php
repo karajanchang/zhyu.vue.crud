9323d6ef0b75d58e38f293b8493365db7a83dfc8
david <karajanchang@gmail.com>
2021-10-18 13:13:18 +0800

    Curl 可以在url後面加上自定義的參數

--- src/Http/Controllers/CrulController.php
+++ src/Http/Controllers/CrulController.php
@@ -1,97 +1,70 @@
 <?php
+namespace App\Http\Controllers\Admin\Announcement;
 
 
-namespace ZhyuVueCurd\Http\Controllers;
-
-
-use Illuminate\Support\Facades\Log;
-use ZhyuVueCurd\Helper\TableServiceBind;
-use ZhyuVueCurd\Helper\TraitLogging;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Board;
+use App\Models\BoardItem;
+use App\Service\Admin\Announcement\BoarditemService;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
+use ZhyuVueCurd\Http\Controllers\CRULInterface;
+use ZhyuVueCurd\Http\Controllers\CRULTrait;
+use ZhyuVueCurd\Http\Controllers\CrulController;
 
-class CrulController extends Controller
+class BoarditemController extends CrulController implements CRULInterface
 {
-    use TraitLogging;
     use CRULTrait;
 
-    public $data_url = null;
+    protected $module = 'admin';
+    protected $tag = 'announcement.boarditem';
 
-    public $create_url = null;
-    public $edit_url = null;
-    public $index_url = null;
-    public $store_url = null;
-
-    public $tableService = null;
-
-    //--預設module
-    protected $module = '';
-
-    //--抓tableServcie用
-    protected $tag = '';
-
-    protected $seconds = 5;
-
-    protected $service = null;
-
-    public function __construct()
+    /*
+     * Service 設定
+     */
+    public function service()
     {
-        if(method_exists($this, 'createUrl')){
-            $this->create_url = $this->createUrl();
-        }
-        if(method_exists($this, 'dataUrl')){
-            $this->data_url = $this->dataUrl();
-        }
-        if(method_exists($this, 'indexUrl')){
-            $this->index_url = $this->indexUrl();
-        }
-        if(method_exists($this, 'storeUrl')){
-            $this->store_url = $this->storeUrl();
-        }
-        $this->seconds = env('ATOMIC_LOCK_A_SECONDS', 5);
 
-        if(method_exists($this, 'service')){
-            $this->service = app($this->service());
-        }
-
-    }
-
-    protected function cacheKey() : string{
-        $auth_user_id = Auth::check() ? Auth::user()->id : '';
-
-        return env('APP_ENV') . md5(get_class($this)). $auth_user_id;
+        return BoarditemService::class;
     }
 
     /*
      * 列表
      */
     public function index(){
-        $this->auth('read');
+        $this->authorize('announcement_board:read');
+
+        $board_id = Request::get('board_id');
+        if(is_null($board_id)){
+
+            return redirect(route('admin.announcement.board.index'));
+        }
 
         $this->tableBind($this->module, $this->tag)->index();
 
-        return $this->view($this->module.'.'.$this->tag.'.index');
+        return $this->view('admin.announcement.boarditem.index', compact('board_id'), Request::all());
     }
 
     /*
      * 新增
      */
     public function create(){
-        $this->auth('read');
+        $this->authorize('announcement_board:create');
 
-        $this->tableBind($this->module, $this->tag)->form();
+        $board_id = (int) Request::get('board_id');
+        $boardItem = new BoardItem();
+        $boardItem->board_id = $board_id;
 
-        return $this->view($this->module.'.'.$this->tag.'.form');
+        $this->tableBind($this->module, $this->tag)->form($boardItem);
+
+        return $this->view('ZhyuVueCurd::'.$this->module.'.'.$this->tag.'.form', ['board_id' => $board_id], Request::all());
     }
 
     /*
      * 儲存
      */
-    public function store(Request $request){
-        $this->auth('create');
+    public function store(\Illuminate\Http\Request $request){
+        $this->authorize('announcement_board:create');
 
         $this->tableBind($this->module, $this->tag)->form();
 
@@ -118,7 +91,7 @@ class CrulController extends Controller
      * 修改
      */
     public function edit(int $id){
-        $this->auth('update');
+        $this->authorize('announcement_board:edit');
 
         $model = app($this->service())->findById($id);
 
@@ -131,17 +104,15 @@ class CrulController extends Controller
 
         $this->setEditUrl(route($this->module.'.'.$this->tag.'.update', [ $tag => $model ]));
 
-        return $this->view($this->module.'.'.$this->tag.'.form');
+        return $this->view($this->module.'.'.$this->tag.'.form', [], Request::all());
     }
-
-
 
     /*
      *  更新
      */
-    public function update(int $id, Request $request)
+    public function update(int $id, \Illuminate\Http\Request $request)
     {
-        $this->auth('update');
+        $this->authorize('announcement_board:edit');
 
         $model = app($this->service())->findById($id);
         $this->tableBind($this->module, $this->tag)->form($model);
@@ -171,7 +142,7 @@ class CrulController extends Controller
      * 刪除
      */
     public function destroy(int $id){
-        $this->auth('delete');
+        $this->authorize('announcement_board:delete');
 
         $service = app($this->service());
         $model = $service->findById($id);
@@ -192,169 +163,5 @@ class CrulController extends Controller
 
             return $this->error($msg);
         }
-    }
-
-    /*
-     * Bind 一個 tatable
-     */
-    protected function tableBind(string $module, string $tag){
-        $this->tableService = TableServiceBind::bind($module, $tag);
-
-        return $this->tableService;
-    }
-
-    /**
-     * 設定 修改submit的連結url
-     * @param null $edit_url
-     */
-    protected function setEditUrl($edit_url): void
-    {
-        $this->edit_url = $edit_url;
-    }
-
-    /*
-     * 駈證是不存在此筆資料
-     */
-    protected function validateModel(Model $row){
-        if(empty($row->id)){
-            return response()->withException(new \Exception('無此資料'));
-        }
-
-        return true;
-    }
-
-    /*
-     * ajax呼叫 返回 錯誤
-     */
-    protected function responseError($message, int $code){
-        if(is_string($message)) {
-
-            return response()->json(compact('message'))->setStatusCode($code);
-        }else{
-
-            return response()->json($message)->setStatusCode($code);
-        }
-        /*
-        return ['error' => [
-            'message' => $message,
-            'code' => $code,
-        ]
-        ];
-        */
-    }
-
-    /*
-     * 返回view
-     */
-    protected function view(string $view_name, array $params = []){
-        $views = explode('.', $view_name);
-//        dump($view_name);
-
-        return view()->first([
-            $view_name,
-            'vendor.curl.'.$views[(count($views)-1)],
-            'ZhyuVueCurd::curl.'.$views[(count($views)-1)],
-        ],
-            array_merge(
-                $params,
-                $this->groupUrls(),
-                [ 'tableService' => $this->tableService ],
-                [ 'row' => $this->tableService->model ]
-            )
-        );
-    }
-
-    /*
-    * 組合url
-    */
-    private function groupUrls() : array{
-
-        return
-            [
-                'urls' => [
-                    'create' => !empty($this->create_url) ? url($this->create_url) : null,
-                    'data' => url($this->data_url),
-                    'edit' => url($this->edit_url),
-                    'index' => url($this->index_url),
-                    'store' => url($this->store_url),
-                ],
-            ];
-    }
-
-    /*
-     * 定義網址 - create
-     */
-    public function createUrl() : string{
-
-        return route($this->module.'.'.$this->tag .'.create');
-    }
-
-    /*
-     * 定義網址 - store
-     */
-    public function storeUrl() : string{
-
-        return route($this->module.'.'.$this->tag .'.store');
-    }
-
-    /*
-     * 定義網址 - data
-     */
-    public function dataUrl() : string{
-//        dump($this->module, $this->tag);
-
-//        dump($a);
-        return route('vendor.ajax.'.$this->module.'.'.$this->tag, [ 'module' => $this->module, 'tag' => $this->tag ]);
-    }
-
-    /*
-    * 定義網址 - index
-    */
-    public function indexUrl() : string{
-
-        return route($this->module.'.'.$this->tag .'.index');
-    }
-
-    protected function error(string $msg = null){
-
-        return $this->responseError($msg, Response::HTTP_BAD_REQUEST);
-        /*
-        return response()->json([
-            'errors' => $msg
-        ],
-            Response::HTTP_BAD_REQUEST);*/
-    }
-
-    public function getModule() : string{
-
-        return $this->module;
-    }
-
-    public function getTag() : string{
-
-        return $this->tag;
-    }
-
-    public function getLastTag(){
-        $tags = explode('.', $this->tag);
-        $array = array_slice($tags, -1);
-
-        return array_pop($array);
-    }
-
-    private function parseTagForDot2Underline(){
-        $tags = explode('.', $this->tag);
-
-        return join('_', $tags);
-    }
-
-    public function getModel(){
-
-        return $this->tableService->model;
-    }
-
-    protected function auth($act){
-        //$tag = $this->getLastTag();
-        $this->authorize($this->parseTagForDot2Underline().':'.$act);
     }
 }
