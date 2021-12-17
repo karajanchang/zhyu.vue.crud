f64cac55c04bc9e0578200b54e701f945bf5d71f
david <karajanchang@gmail.com>
2021-12-17 09:30:08 +0800

    [menu] style remove z-index:299

--- src/views/livewire/page/menu.blade.php
+++ src/views/livewire/page/menu.blade.php
@@ -1,6 +1,6 @@
 <section class="is-hidden-mobile">
     <div class="p-1 is-pulled-right">
-        <div class="container is-fluid is-size-6" style="z-index: 299">
+        <div class="container is-fluid is-size-6">
             @if(isset($menus) && $menus->count() > 0)
                 @foreach($menus as $menu)
                     @php
