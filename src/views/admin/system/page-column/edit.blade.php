afe3a3c66c614ffb2f5272bbc4b84b39a78fd482
david <karajanchang@gmail.com>
2024-07-15 11:41:45 +0800

    [http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css to https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css]

--- src/views/admin/system/page-column/edit.blade.php
+++ src/views/admin/system/page-column/edit.blade.php
@@ -10,7 +10,7 @@
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')  }}">
     <!-- IonIcons -->
-    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('vendor/css/adminlte.min.css')}}">
     <!-- buefy--->
