ffc61589d7d96bcf14dd56e7b7057d636a180875
david <karajanchang@gmail.com>
2024-07-15 12:27:03 +0800

    [修正網址判斷的問題]

--- src/functions/functions.php
+++ src/functions/functions.php
@@ -3,7 +3,7 @@
 //--看是否符合url  (把第一個 /admin/system  ==> admin/system*)
 function ifMatchUrl($url){
     $url = preg_replace('/^\//', '', $url);
-    if(\Request::is($url.'*')){
+    if(\Request::is($url.'/*') || \Request::is($url)){
         return true;
     }
     return false;
